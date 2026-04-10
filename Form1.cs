44257de0b759d0e8d0bd77a2000742d9f9b7d2af
INFINIX <INFINIX@XBOOK_B14>
2026-04-10 14:13:14 +0700

    Add project files.

---
+++ Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace PraktikumADO
+{
+    public partial class Form1 : Form
+    {
+        SqlConnection conn;
+        SqlCommand cmd;
+
+        private void koneksi()
+        {
+            string connectionString = "Data Source=XBOOK_B14\\SQLEXPRESS;Initial Catalog=PraktikumADO;Integrated Security=True";
+            conn = new SqlConnection(connectionString);
+        }   
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
