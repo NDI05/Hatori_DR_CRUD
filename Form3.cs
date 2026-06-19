2b9ae3ee7c90bea4c605d6eaa410c8b8ae7e9b29
ndi05 <andhikap877@gmail.com>
2026-06-20 02:38:28 +0700

    feat: menambahkan Form3 untuk preview Crystal Report

---
+++ Form3.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PraktikumADO
+{
+    public partial class Form3 : Form
+    {
+        DAL dbLogic = new DAL();
+
+        SqlDataAdapter da;
+        DataTable dtMahasiswa;
+
+        CrystalReport1 listMahasiswa = new CrystalReport1();
+
+        string prodi { get; set;  }
+
+        DateTime tglmasuk { get; set; }
+        public Form3(string Prodi, DateTime TglMasuk)
+        {
+            InitializeComponent();
+            prodi = Prodi;
+            tglmasuk = TglMasuk;
+
+            try
+            { 
+                DataTable dtMahasiswa = dbLogic.getDataRekap(prodi, tglmasuk);
+                listMahasiswa.SetDataSource(dtMahasiswa);
+                crystalReportViewer1.ReportSource = listMahasiswa;
+                crystalReportViewer1.Refresh();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Gagal Load Data:" + ex.Message);
+            }
+    }
+}
+}
